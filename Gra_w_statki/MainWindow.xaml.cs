f8757386a75fd39b85728d30d6be8506b6d67162
Tadaj24 <34206943+Tadaj24@users.noreply.github.com>
2018-01-20 15:20:51 +0100

    1517_2001
    +pierwsza wersja okno gry

--- Gra_w_statki/MainWindow.xaml.cs
+++ Gra_w_statki/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gra_w_statki
 {
@@ -24,5 +14,59 @@ namespace Gra_w_statki
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var powt = 10;
+            //dodaje kolumny nowe
+            for (int i = 0; i < powt + 1; i++)
+            {
+                OurBoard.RowDefinitions.Add(new RowDefinition());
+                OurBoard.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 1; i < powt + 1; i++)
+            {
+                TextBlock textblockx = new TextBlock()
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Text = Convert.ToString(i),
+                    //FontSize = 20,
+                    Margin = new Thickness(1)
+                };
+                Grid.SetRow(textblockx, 0);
+                Grid.SetColumn(textblockx, i);
+                this.OurBoard.Children.Add(textblockx);
+
+                TextBlock textblocky = new TextBlock()
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Text = Convert.ToString(Convert.ToChar(64 + i)),
+                    //FontSize = 20,
+                    Margin = new Thickness(1)
+                };
+                Grid.SetRow(textblocky, i);
+                Grid.SetColumn(textblocky, 0);
+                this.OurBoard.Children.Add(textblocky);
+            }
+            // tworzy buttony dynamicznie
+            for (int i = 1; i < powt + 1; i++)
+            {
+                for (int j = 1; j < powt + 1; j++)
+                {
+                    Button guzik = new Button();
+                    //guzik.Content = "x" + i + "y" + j;
+                    guzik.Margin = new Thickness(1);
+                    guzik.Name = "x" + i + "y" + j;
+                    Grid.SetRow(guzik, i);
+                    Grid.SetColumn(guzik, j);
+                    this.OurBoard.Children.Add(guzik);
+                }
+            }            
+        }
+
+
     }
 }
